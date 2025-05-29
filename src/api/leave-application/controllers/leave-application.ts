0f4e00d6bc425dbba4e79bc208fa8323566e96fa
ha nguyen thanh <ha.nguyenthanh@asnet.com.vn>
2025-05-29 17:38:52 +0700

    Initial commit for Strapi backend

---
+++ src/api/leave-application/controllers/leave-application.ts
@@ -0,0 +1,7 @@
+/**
+ * leave-application controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::leave-application.leave-application');
---
+++ src/api/leave-application/routes/leave-application.ts
@@ -0,0 +1,7 @@
+/**
+ * leave-application router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::leave-application.leave-application');
