43813bcd349721ad8c32129a7efdc9832ec74777
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-03 11:14:55 +0700

    Update database

--- config/database.ts
+++ config/database.ts
@@ -1,44 +1,61 @@
 import path from "path";
-import { parse } from "pg-connection-string";
 
 export default ({ env }) => {
   const client = env("DATABASE_CLIENT", "postgres");
 
-  if (client === "postgres") {
-    const dbUrl = env("DATABASE_URL");
-
-    if (!dbUrl) {
-      console.error("❌ DATABASE_URL is missing!");
-      throw new Error("Missing DATABASE_URL environment variable");
-    }
-
-    const config = parse(dbUrl);
-
-    return {
+  const connections = {
+    mysql: {
       connection: {
-        client: "postgres",
-        connection: {
-          host: config.host,
-          port: Number(config.port),
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: false,
-          },
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
         },
-        pool: {
-          min: 0,
-          max: 2,
-        },
-        acquireConnectionTimeout: 60000,
       },
-    };
-  }
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
+    postgres: {
+      connection: {
+        connectionString: env("DATABASE_URL"),
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
 
-  return {
-    connection: {
-      client: "sqlite",
+    sqlite: {
       connection: {
         filename: path.join(
           __dirname,
@@ -48,6 +65,14 @@ export default ({ env }) => {
         ),
       },
       useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
   };
 };
